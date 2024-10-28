e816a8f0c56e24ab2b2cbba1ed2066ebb7752326
syed1006 <syedsalman1006@gmail.com>
2024-10-28 16:45:34 +0530

    added infinite moving cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import TaxiBookingForm from "./components/forms/taxi-booking";
 import { ContainerScroll } from "./components/globals/container-scroll-animation";
+import { InfiniteMovingCards } from "./components/globals/infinite-moving-cards";
 import { LampComponent } from "./components/globals/lamp";
 import BangaloreIcon from "./components/icons/bangalore-icon";
 import DelhiIcon from "./components/icons/delhi-icon";
@@ -9,8 +10,13 @@ import KolkattaIcon from "./components/icons/kolkatta-icon";
 import MumbaiIcon from "./components/icons/mumbai-icon";
 
 function App() {
+	const items = [
+		{ icon: <KolkattaIcon width={290} height={290} /> },
+		{ icon: <MumbaiIcon width={290} height={290} /> },
+		{ icon: <HyderabadIcon width={290} height={290} /> },
+	];
 	// return <ContainerScroll bodyComponent={<TaxiBookingForm />} />;
-	return <KolkattaIcon width={290} height={290} />;
+	return <InfiniteMovingCards items={items} speed="normal" />;
 }
 
 export default App;
