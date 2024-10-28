5dca21b236674e6d5e064c93955f002b2567a4d9
syed1006 <syedsalman1006@gmail.com>
2024-10-28 16:30:03 +0530

    added icons for cities

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import "./App.css";
 import TaxiBookingForm from "./components/forms/taxi-booking";
 import { ContainerScroll } from "./components/globals/container-scroll-animation";
 import { LampComponent } from "./components/globals/lamp";
+import BangaloreIcon from "./components/icons/bangalore-icon";
+import DelhiIcon from "./components/icons/delhi-icon";
+import HyderabadIcon from "./components/icons/hyderabad-icon";
+import KolkattaIcon from "./components/icons/kolkatta-icon";
+import MumbaiIcon from "./components/icons/mumbai-icon";
 
 function App() {
-	return <ContainerScroll bodyComponent={<TaxiBookingForm />} />;
+	// return <ContainerScroll bodyComponent={<TaxiBookingForm />} />;
+	return <KolkattaIcon width={290} height={290} />;
 }
 
 export default App;
